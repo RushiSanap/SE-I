e7fe0fe841360de4faaa26caaddc6bbc1d0a2ae0
Vishwesh Pujari <vvp8793@gmail.com>
2021-11-11 13:15:51 +0530

    handle exception from evaluate in testing.py

--- testing.py
+++ testing.py
@@ -1,6 +1,5 @@
 from random import randint, choice, uniform
 from sys import argv
-from random import randint, choice
 
 argc = len(argv)
 if argc < 2:
@@ -20,16 +19,19 @@ def testExpr(expr: str) -> bool:
     flag = 0
     try:
         ans = eval(expr.replace("^", "**"))
-        flag = 1
     except:
+        flag = 1
         return None
     
-    if( flag == 1 ):
-        if format(evaluate(expr), ".4f") == format(ans, ".4f") :
-            return True
-        else:
+    if( flag == 0 ):
+        try:
+            if format(evaluate(expr), ".4f") == format(ans, ".4f") :
+                return True
+            else:
+                return False
+        except: # some exception happened in evaluate()
             return False
-
+    
 if customExpr != None:
     success = testExpr(customExpr)
     if success == None:
@@ -40,7 +42,10 @@ if customExpr != None:
     elif not success:
         print("Failed")
         print("Expression:", customExpr)
-        print("Your output:", evaluate(customExpr)," Expected Output:", eval(customExpr))
+        try:
+            print("Your output:", evaluate(customExpr)," Expected Output:", eval(customExpr))
+        except Exception as e:
+            print("Your output:", e, " Expected Output:", eval(customExpr))
     exit()
 
 ops = ["+", "-", "*", "/", "%"]#, "^"]
@@ -78,29 +83,18 @@ while i < 15:
 
     for k in range( op_br - cl_br):
         expr += " " + ")"
-
+    #print(expr)
     success = testExpr(expr)
+    #print(success)
+    #exit()
     if success == True:
         print("\nSuccessful for Test case no.", i+1)
         i += 1
     elif success == False:
         print("\nFail for Testcase no.", i+1)
         print("Expression:", expr)
-        print("Your output:", evaluate(expr)," Expected Output:", eval(expr))
+        try:
+            print("Your output:", evaluate(expr)," Expected Output:", eval(expr))
+        except Exception as e:
+            print("Your output:", e," Expected Output:", eval(expr))
         i += 1
-    expr_1 = expr.replace("^", "**")
-    flag = 0
-
-    try:
-        ans = eval( expr_1)
-        flag = 1
-    except:
-        pass
-
-    if( flag == 1 ):
-        if evaluate(expr) == ans :
-            print("\nSuccessful for Test case no.", i+1)
-        else:
-            print("\nFail for Testcase no.", i+1)
-            print("Expression:", expr)
-            print("Your output:", evaluate(expr)," Expected Output:", ans )    
